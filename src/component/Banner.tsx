a4ecba274b62fd245873db8b12e22dd4b87ec98d
Nhat Quach <nhatqm@unitech.vn>
2025-03-06 16:33:20 +0700

    enhance Banner component layout; add right section with contact info

--- src/component/Banner.tsx
+++ src/component/Banner.tsx
@@ -1,12 +1,13 @@
 export default function Banner() {
   return (
     <div className="w-screen h-[720px] py-24 px-26 flex flex-wrap justify-between items-end bg-[url(/images/banner1.jpg)] bg-cover bg-center bg-no-repeat">
+      {/* left  */}
       <div className="flex flex-col gap-2">
         <h1 className="text-5xl font-bold text-white text-[120px] font-big-shoulders">
           GO-KARTS
         </h1>
-        <div className="max-w-[552.25 px] flex justify-between">
-          <p className="text-white mt-4 max-w-[347 px]">
+        <div className="max-w-[552.25px] flex justify-between">
+          <p className="text-white mt-4 max-w-[347px]">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. In euismod
             ipsum et dui rhoncus, nec ultrices urna scelerisque. Nam sit amet
             nisi nec velit aliquet ultricies. Donec nec nunc nec nunc.
@@ -16,6 +17,20 @@ export default function Banner() {
               Find out more
             </div>
           </button>
+        </div>
+      </div>
+      {/* right  */}
+      <div className="w-[573px] flex flex-col items-end gap-[100px]">
+        <div className="flex flex-col items-end gap-[-20px]">
+          <div className="flex justify-center items-center gap-2.5 bg-[#d9d9d9] pl-3 pr-1 pt-1 pb-1 rounded-[50px]">
+            <span className="font-bold text-base text-[#292929]">
+              +420737878891
+            </span>
+            <div className="w-[30px] h-[30px] flex justify-center items-center bg-[#e30613] px-3 py-1.5 rounded-[50px]">
+              <svg className="w-3.5 h-3.5"></svg>
+            </div>
+          </div>
+          <img className="w-[87px] h-[87px] object-cover"></img>
         </div>
       </div>
     </div>
